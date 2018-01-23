1e3858a74edf57d245d018fd5bd2121aef18699f
JaretWright <hkkA223>
2018-01-23 10:13:14 -0500

    added snowDepth

--- src/w18jan23comp1008/W18Jan23COMP1008.java
+++ src/w18jan23comp1008/W18Jan23COMP1008.java
@@ -18,6 +18,14 @@ public class W18Jan23COMP1008 {
        //challenge 2 & 3 
        for (int counter=0; counter <= 100; counter += 10)
            System.out.printf("%d%s%n", counter, counter%20==0?" Java":"");
+       
+       System.out.printf("For snow depth 20, can we ski? %b%n", timeToSki(20));
+       System.out.printf("For snow depth 40, can we ski? %b%n", timeToSki(40));
     }
     
+    //challenge 4 - time to ski method
+    public static boolean timeToSki(int snowDepth)
+    {
+        return snowDepth>30;
+    }
 }
